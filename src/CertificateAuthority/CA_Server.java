8d8cd3b08f3318f3d2b1c1803027cd9728d59ded
ZaherAirout <m.zaher.airout@gmail.com>
2017-12-12 04:53:11 +0200

    add Certificate Authority Server

---
+++ src/CertificateAuthority/CA_Server.java
@@ -0,0 +1,130 @@
+package CertificateAuthority;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.*;
+import java.security.cert.X509Certificate;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.net.ServerSocketFactory;
+
+import asymmetricserver.Server;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+public class CA_Server {
+
+    private static ServerSocket serverSocket;
+    static Vector<String> users = new Vector<>();
+    static Vector<ClientHandler> clients = new Vector<>();
+    static Vector<X509Certificate> certs = new Vector<>();
+    static KeyPair pair;
+
+    public static void main(String... args) throws IOException {
+        KeyPairGenerator keyGen;
+        try {
+            // create the keys
+            keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(1024);
+            pair = keyGen.generateKeyPair();
+        } catch (Exception ex) {
+            Logger.getLogger(CA_Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        serverSocket = ServerSocketFactory.getDefault().createServerSocket(20000);
+        while (true) {
+            try {
+                final Socket socketToClient = serverSocket.accept();
+                System.out.println("client accepted");
+                ClientHandler clientHandler = new ClientHandler(socketToClient);
+                clients.add(clientHandler);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static class ClientHandler extends Thread {
+
+        String user;
+        ObjectInputStream input;
+        ObjectOutputStream output;
+
+        ClientHandler(Socket socket) throws Exception {
+            try {
+                input = new ObjectInputStream(socket.getInputStream());
+                output = new ObjectOutputStream(socket.getOutputStream());
+            } catch (IOException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            user = recieve().toString();
+            users.add(user);
+            PublicKey publicKey = (PublicKey) recieve();
+
+            // generate the certificate
+            Security.addProvider(new BouncyCastleProvider());
+            X509Certificate cert = X509V1CreateExample.generateV1Certificate(publicKey, pair.getPrivate(), user);
+            send(cert);
+            certs.add(cert);
+        }
+
+        void setUser(String user) {
+            this.user = user;
+        }
+
+        Object recieve() {
+            Object o = new Object();
+            try {
+                o = input.readObject();
+            } catch (IOException ex) {
+                return null;
+//                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return o;
+        }
+
+        void send(Object text) {
+            try {
+                output.writeObject(text);
+            } catch (IOException ex) {
+//                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        void broadcast(Object text) {
+            for (ClientHandler clientHandler : clients) {
+                if (!clientHandler.user.equals(user)) {
+                    clientHandler.send(text);
+                }
+            }
+        }
+
+        void broadcast(Object text, Object tag) {
+            for (ClientHandler clientHandler : clients) {
+                if (!clientHandler.user.equals(user)) {
+                    clientHandler.send(tag);
+                    clientHandler.send(text);
+                }
+            }
+        }
+
+        void sendToUser(Object text, Object user, Object tag) {
+            for (ClientHandler clientHandler : clients) {
+                if (clientHandler.user.equals(user.toString())) {
+                    clientHandler.send(tag);
+                    clientHandler.send(text);
+                }
+            }
+        }
+
+        @Override
+        public void run() {
+        }
+    }
+}
---
+++ src/CertificateAuthority/X509V1CreateExample.java
@@ -0,0 +1,70 @@
+package CertificateAuthority;
+
+
+import java.math.BigInteger;
+import java.security.*;
+import java.security.cert.X509Certificate;
+import java.util.Date;
+
+import javax.security.auth.x500.X500Principal;
+import org.bouncycastle.asn1.x509.X509Name;
+import org.bouncycastle.jce.X509Principal;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import org.bouncycastle.x509.X509V1CertificateGenerator;
+
+/*
+import static serverchatclient.EncryptionUtil.ALGORITHM;
+import static serverchatclient.EncryptionUtil.PRIVATE_KEY_FILE;
+import static serverchatclient.EncryptionUtil.PUBLIC_KEY_FILE;
+import static serverchatclient.EncryptionUtil.areKeysPresent;
+import static serverchatclient.EncryptionUtil.decrypt;
+import static serverchatclient.EncryptionUtil.encrypt;
+import static serverchatclient.EncryptionUtil.generateKey;
+*/
+
+/**
+ * Basic X.509 V1 Certificate creation.
+ */
+public class X509V1CreateExample {
+
+    public static X509Certificate generateV1Certificate(PublicKey PK, PrivateKey PVK, String Name)
+            throws InvalidKeyException, SignatureException {
+        // generate the certificate
+        Security.addProvider(new BouncyCastleProvider());
+        X509V1CertificateGenerator certGen = new X509V1CertificateGenerator();
+
+        certGen.setSerialNumber(BigInteger.valueOf(System.currentTimeMillis()));
+        certGen.setIssuerDN(new X509Principal("CN=SERVER"));
+        certGen.setNotBefore(new Date(System.currentTimeMillis() - 50000));
+        certGen.setNotAfter(new Date(System.currentTimeMillis() + 50000));
+        certGen.setSubjectDN(new X509Name("CN=" + Name));
+        certGen.setPublicKey(PK);
+
+// i get error here
+        certGen.setSignatureAlgorithm("SHA256WithRSAEncryption");
+
+        return certGen.generateX509Certificate(PVK);
+    }
+
+    public static void main(String[] args) throws Exception {
+        // create the keys
+        final KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(1024);
+        KeyPair pair = keyGen.generateKeyPair();
+        KeyPair pair2 = keyGen.generateKeyPair();
+
+        // generate the certificate
+        Security.addProvider(new BouncyCastleProvider());
+        X509Certificate cert = generateV1Certificate(pair2.getPublic(), pair.getPrivate(), "eid");
+        
+        
+        cert.verify(pair.getPublic());
+        // show some basic validation
+        cert.checkValidity(new Date());
+
+//        cert.verify(cert.getPublicKey());
+
+        // System.out.println("valid certificate generated");*/
+    }
+}
